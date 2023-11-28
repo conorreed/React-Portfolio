fb75ff187c464b6c0fce5639d22c9332d93f010d
Conor <conor.reed@gmail.com>
2023-11-27 18:45:36 -0600

    added pictures and github/deployment links to the portfolio page

--- Conor-Reed-Portfolio/src/pages/Portfolio.jsx
+++ Conor-Reed-Portfolio/src/pages/Portfolio.jsx
@@ -1,14 +1,43 @@
-import Project from './Project'; 
+import Project from "./Project";
 
 function Portfolio() {
   const projects = [
     {
-      title: 'Project 1',
-      imageSrc: '/project1.jpg',
-      deployedLink: 'https://example.com/project1',
-      githubLink: 'https://github.com/example/project1',
+      title: "Europe in Your Pocket",
+      imageSrc: "/project1.jpg",
+      deployedLink: "https://crcarmen23.github.io/europe-in-your-pocket/",
+      githubLink: "https://github.com/conorreed/europe-in-your-pocket",
     },
-    // Add more projects as needed
+    {
+      title: "Weather Dashboard",
+      imageSrc: "/project1.jpg",
+      deployedLink: "https://conorreed.github.io/Weather-Dashboard/",
+      githubLink: "https://github.com/conorreed/Weather-Dashboard",
+    },
+    {
+      title: "Work Day Planner",
+      imageSrc: "/project1.jpg",
+      deployedLink: "https://conorreed.github.io/work-day-planner/",
+      githubLink: "https://github.com/conorreed/work-day-planner",
+    },
+    {
+      title: "Password Generator",
+      imageSrc: "/project1.jpg",
+      deployedLink: "https://conorreed.github.io/Password-Generator/",
+      githubLink: "https://github.com/conorreed/Password-Generator",
+    },
+    {
+      title: "SVG Logo Generator",
+      imageSrc: "/project1.jpg",
+      deployedLink: "https://youtu.be/maO6HRFl980",
+      githubLink: "https://github.com/conorreed/SVG-Logo-Generator",
+    },
+    {
+      title: "Note Taker App",
+      imageSrc: "/project1.jpg",
+      deployedLink: "https://example.com/project1",
+      githubLink: "https://github.com/example/project1",
+    },
   ];
 
   return (
