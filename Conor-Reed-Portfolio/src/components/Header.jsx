ef72b6602417ee066a69eb14220dca4061f6a718
Conor <conor.reed@gmail.com>
2023-11-27 19:18:10 -0600

    created header

---
+++ Conor-Reed-Portfolio/src/components/Header.jsx
@@ -0,0 +1,13 @@
+// Header.jsx
+import Navigation from './Navigation';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Your Name</h1>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
