23e57d89a05c213e5df4d66c9aa9876ab3bb6ca0
Conor <conor.reed@gmail.com>
2023-11-27 18:17:55 -0600

    created the about me page

---
+++ Conor-Reed-Portfolio/src/pages/AboutMe.jsx
@@ -0,0 +1,16 @@
+export default function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <img src="/AboutMePhoto.jpg" alt="Profile" />
+            <p>
+                Hey, my name is Conor Reed and I have always had a passion for learning and competition. From a young age I was
+                interested in learning about the past and finding ways to relate it to the present. It came as no surprise when I decided to get a bachelors degree in History. 
+                While in community college I had dabbled in programming courses where I learned the basics of C++. At the time, this did not seem like the correct path because
+                I was not able to see myself using it. Recently, I completed a 3 month fullstack web development bootcamp where I was able to recieve comprehensive training 
+                in many different technologies such as html, css, JavaScript, mySQL, MongoDB, React, and others. What began as a path for a stable career ended up sparking a desire to learn more
+                while continuing to grow as a web developer. 
+            </p>
+        </div>
+    );
+}
