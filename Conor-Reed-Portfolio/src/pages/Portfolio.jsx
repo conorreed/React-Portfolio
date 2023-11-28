c17236e6903e74b5e1dbfb97387968189f100b46
Conor <conor.reed@gmail.com>
2023-11-27 18:52:06 -0600

    added another picture and also linked to the pictures on the portfolio page

--- Conor-Reed-Portfolio/src/pages/Portfolio.jsx
+++ Conor-Reed-Portfolio/src/pages/Portfolio.jsx
@@ -4,39 +4,39 @@ function Portfolio() {
   const projects = [
     {
       title: "Europe in Your Pocket",
-      imageSrc: "/project1.jpg",
+      imageSrc: "/EuropePocketPicture.jpg",
       deployedLink: "https://crcarmen23.github.io/europe-in-your-pocket/",
       githubLink: "https://github.com/conorreed/europe-in-your-pocket",
     },
     {
       title: "Weather Dashboard",
-      imageSrc: "/project1.jpg",
+      imageSrc: "/WeatherDashboardPicture.png",
       deployedLink: "https://conorreed.github.io/Weather-Dashboard/",
       githubLink: "https://github.com/conorreed/Weather-Dashboard",
     },
     {
       title: "Work Day Planner",
-      imageSrc: "/project1.jpg",
+      imageSrc: "/WorkDayPlannerPicture.png",
       deployedLink: "https://conorreed.github.io/work-day-planner/",
       githubLink: "https://github.com/conorreed/work-day-planner",
     },
     {
       title: "Password Generator",
-      imageSrc: "/project1.jpg",
+      imageSrc: "/PasswordGeneratorPicture.png",
       deployedLink: "https://conorreed.github.io/Password-Generator/",
       githubLink: "https://github.com/conorreed/Password-Generator",
     },
     {
       title: "SVG Logo Generator",
-      imageSrc: "/project1.jpg",
+      imageSrc: "/SVGLogoGeneratorPicture.png",
       deployedLink: "https://youtu.be/maO6HRFl980",
       githubLink: "https://github.com/conorreed/SVG-Logo-Generator",
     },
     {
       title: "Note Taker App",
-      imageSrc: "/project1.jpg",
-      deployedLink: "https://example.com/project1",
-      githubLink: "https://github.com/example/project1",
+      imageSrc: "/NoteTakerAppPicture.png",
+      deployedLink: "https://secret-waters-66437-1e6b11615e1a.herokuapp.com/",
+      githubLink: "https://github.com/conorreed/Note-Taker-App",
     },
   ];
 
